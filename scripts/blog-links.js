761a17d348475ba96e9f610309414c65700bb21b
Brian Wagner <briwagner@outlook.com>
2015-10-24 00:21:21 -0400

    Add blog 4 to blog links angular

--- scripts/blog-links.js
+++ scripts/blog-links.js
@@ -1,10 +1,14 @@
 var blogPosts = [
 	{
-		href: "css-concepts.html",
+    href: "array-hash.html",
+    title: "What's on Your Keychain?"
+  },
+	{
+    href: "css-concepts.html",
 		title: "Printing Press to CSS"
 	},
 	{
 		href: "git.html",
 		title: "The Benefits of Control"
 	}
-]; 
+];
